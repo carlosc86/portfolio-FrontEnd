bb2dfa9ff74c7c680355498f852c7256f07f2453
Carlos A. Carricondo <c.carricondo@gmail.com>
2022-05-15 21:07:48 -0300

    Modificadas algunas variables de entorno para generar el deploy

--- FrontEnd-Angular/src/environments/environment.prod.ts
+++ FrontEnd-Angular/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  urlServicio:"https://app.heroku.com/miServicio",
+  urlServicio:"http://portfolio-ap-backend.herokuapp.com",
   endpoint_seccion:"/secciones",
   endpoint_estudios:"/estudios",
   endpoint_habilidades:"/habilidades",
